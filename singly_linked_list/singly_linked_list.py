09f7666dfd642eef9f408aef4640dbf007de4175
DentonWarnock <dentonwarnock@gmail.com>
2020-07-22 19:02:20 -0700

    completed single linked list and tested all methods.

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -42,6 +42,7 @@ class LinkedList:
             value = self.head.get_value()
             self.head = None
             self.tail = None    
+            self.length -= 1
             return value        
         # list with +2 Nodes
         else:
@@ -51,12 +52,45 @@ class LinkedList:
             return value
 
     def remove_tail(self):
-        pass
+        # empty SLL
+        if self.head is None:
+            return None
+        elif self.head == self.tail:
+            value = self.tail.get_value()
+            self.head = None
+            self.tail = None
+            self.length -= 1    
+            return value 
+        # list with +2 Nodes
+        else:
+            current_node = self.head
+            while current_node.get_next() is not self.tail:
+                current_node = current_node.get_next()
+            value = self.tail.get_value()
+            self.tail = current_node
+            self.tail.set_next(None)
+            self.length -= 1
+            return value
+        
     
     def contains(self, value):
-        pass
+        if self.head is None:
+            return False        
+        #Loop through each node until we see the value, or we cannot go further
+        current_node = self.head        
+        while current_node is not None:
+            #check if this is the node we are looking for
+            if current_node.get_value() == value:
+                return True            
+            #otherwise, go to the next node
+            current_node = current_node.get_next()
+        return False
     
     def get_max(self):
+        # empty list
+        if self.head is None: 
+            return None
+        # non-empty list
         # iterate through all items
         current_node = self.head
         current_max = self.head.get_value()
@@ -64,3 +98,26 @@ class LinkedList:
             if current_node.get_value() > current_max:
                 current_max = current_node.get_value()
             current_node = current_node.get_next()
+            
+        return current_max
+    
+linked_list = LinkedList()
+    
+linked_list.add_to_head(0)
+linked_list.add_to_head(1)
+print(f'does LL contain 0? {linked_list.contains(0)}')
+print(f'does LL contain 1? {linked_list.contains(1)}')
+print(f'does LL contain 2? {linked_list.contains(2)}')
+
+linked_list.add_to_head(5)
+print(f'the start of the list is: {linked_list.head.value}')
+print(f'the max of the list is: {linked_list.get_max()}')
+
+linked_list.remove_head()
+linked_list.remove_tail()
+print(f'the start of the list is: {linked_list.head.value}')
+print(f'the max of the list is: {linked_list.get_max()}')
+
+linked_list.add_to_tail(7)
+print(f'the end of the list is: {linked_list.tail.value}')
+print(f'the max of the list is: {linked_list.get_max()}')
