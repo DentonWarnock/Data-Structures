11c0736571b7593aa01bac4f4915fefc7edec02d
DentonWarnock <dentonwarnock@gmail.com>
2020-07-22 19:48:15 -0700

    refactored stack class to use SLL, all tests passing.

--- stack/stack.py
+++ stack/stack.py
@@ -34,4 +34,34 @@ class Stack:
             removed_item = self.storage.pop()
             return removed_item
         
+        
+# Stack using SLL
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.tail =  None
+        self.size = 0
+        
+    def __len__(self):
+        return self.size
+        
+    def push(self, value):
+        self.size += 1
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
+            
+    def pop(self):
+        if self.head is None:
+            return None
+        else:
+            self.size -= 1
+            removed_value = self.head.value
+            self.head = self.head.get_next()
+            return removed_value
+        
+        
     
